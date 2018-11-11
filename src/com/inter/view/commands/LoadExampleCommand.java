d3446029fd433707f8480f41b15be2151be432d5
axtrm <umihnea52@gmail.com>
2018-11-11 13:41:29 +0200

    Implements example program submenu
    This commit introduces a simple sub-menu into LoadExampleCommand and implements the selectProgram(int) which returns a sample Program.
    The reasons for this commit are: a source code parser is out of the scope of the project, we need to extend the number of supported hard-coded examples.

--- src/com/inter/view/commands/LoadExampleCommand.java
+++ src/com/inter/view/commands/LoadExampleCommand.java
@@ -1,14 +1,14 @@
 package com.inter.view.commands;
 
 import com.inter.controller.Controller;
+import com.inter.exceptions.ExitSignal;
 import com.inter.exceptions.InterpreterException;
 import com.inter.model.expressions.ArithmeticExpression;
 import com.inter.model.expressions.ConstantExpression;
 import com.inter.model.expressions.VariableExpression;
-import com.inter.model.statements.AssignmentStatement;
-import com.inter.model.statements.CompoundStatement;
-import com.inter.model.statements.PrintStatement;
-import com.inter.model.statements.Statement;
+import com.inter.model.statements.*;
+
+import java.util.Scanner;
 
 public class LoadExampleCommand extends Command {
 
@@ -19,47 +19,98 @@ public class LoadExampleCommand extends Command {
         this.controller = controller;
     }
 
-    @Override
-    public void execute() throws InterpreterException {
-        controller.setProgram(this.getExample());
-        System.out.println("Loaded example command.");
+    private Statement loop() throws ExitSignal {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println("Choose a program (or 0, to exit):");
+            System.out.print(">> ");
+            int key = in.nextInt();
+
+            if (key == 0)
+                throw new ExitSignal();
+
+            Statement program = selectProgram(key);
+
+            if (program == null) {
+                System.out.println("Program not implemented.");
+                continue;
+            }
+
+            return program;
+        }
     }
 
-    private Statement getExample() {
-        return new CompoundStatement(
-                new AssignmentStatement(
-                        "a",
-                        new ArithmeticExpression(
-                                '+',
-                                new ConstantExpression(2),
-                                new ArithmeticExpression(
-                                        '*',
-                                        new ConstantExpression(3),
-                                        new ConstantExpression(5)
-                                )
-                        )
-                ),
-                new CompoundStatement(
-                        new AssignmentStatement("b",
-                                new ArithmeticExpression(
-                                        '+',
-                                        new ArithmeticExpression(
-                                                '-',
-                                                new VariableExpression("a"),
-                                                new ArithmeticExpression(
-                                                        '/',
-                                                        new ConstantExpression(4),
-                                                        new ConstantExpression(2)
-                                                )
-                                        ),
-                                        new ConstantExpression(7)
+    @Override
+    public void execute() throws InterpreterException {
+        try {
+            Statement program = this.loop();
+            controller.setProgram(program);
+            System.out.println("Loaded example command.");
+        } catch (ExitSignal es) {
+            System.out.println("Exited example submenu.");
+        }
+    }
+
+    private Statement selectProgram(int index) {
+        switch (index) {
+            case 1: {
+                return new CompoundStatement(
+                        new AssignmentStatement("a",
+                                new ArithmeticExpression('+',
+                                        new ConstantExpression(2),
+                                        new ArithmeticExpression('*',
+                                                new ConstantExpression(3),
+                                                new ConstantExpression(5)
+                                        )
                                 )
                         ),
-                        new PrintStatement(
-                                new VariableExpression("b")
+                        new CompoundStatement(
+                                new AssignmentStatement("b",
+                                        new ArithmeticExpression('+',
+                                                new ArithmeticExpression('-',
+                                                        new VariableExpression("a"),
+                                                        new ArithmeticExpression('/',
+                                                                new ConstantExpression(4),
+                                                                new ConstantExpression(2)
+                                                        )
+                                                ),
+                                                new ConstantExpression(7)
+                                        )
+                                ),
+                                new PrintStatement(
+                                        new VariableExpression("b")
+                                )
                         )
-                )
-        );
+                );
+            }
+            case 2: {
+                /* a = 2 - 2;
+                 * IF(a) THEN(
+                 *    v = 2
+                 * ) ELSE(
+                 *    v = 3
+                 * );
+                 * PRINT(v) */
+                return new CompoundStatement(
+                        new AssignmentStatement(
+                                "a",
+                                new ArithmeticExpression('-',
+                                        new ConstantExpression(2),
+                                        new ConstantExpression(2)
+                                )),
+                        new CompoundStatement(
+                                new IfStatement(
+                                        new VariableExpression("a"),
+                                        new AssignmentStatement("v", new ConstantExpression(2)),
+                                        new AssignmentStatement("v", new ConstantExpression(3))
+                                ),
+                                new PrintStatement(new VariableExpression("v"))
+                        )
+                );
+            }
+            default:
+                return null;
+        }
     }
 
 }
