c5a10061aff63bd386f5ac5ebab9a94445f5cb4a
axtrm <umihnea52@gmail.com>
2018-11-21 12:36:08 +0200

    Implement Boolean expressions
    Supporting ==, !=, <, >, <=, >=

---
+++ src/com/inter/model/expressions/BooleanExpression.java
@@ -0,0 +1,53 @@
+package com.inter.model.expressions;
+
+import com.inter.exceptions.EvaluationException;
+import com.inter.utils.adt.IDictionary;
+
+public class BooleanExpression extends Expression {
+    private String operand;
+    private Expression lhs;
+    private Expression rhs;
+
+    public BooleanExpression(String operand, Expression lhs, Expression rhs) {
+        this.operand = operand;
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    @Override
+    public int evaluate(IDictionary<String, Integer> symTable, IDictionary<Integer, Integer> heapTable) throws EvaluationException {
+        int a = lhs.evaluate(symTable, heapTable);
+        int b = rhs.evaluate(symTable, heapTable);
+        boolean r;
+
+        switch (operand) {
+            case "==":
+                r = (a == b);
+                break;
+            case "!=":
+                r = (a != b);
+                break;
+            case "<":
+                r = (a < b);
+                break;
+            case ">":
+                r = (a > b);
+                break;
+            case "<=":
+                r = (a <= b);
+                break;
+            case ">=":
+                r = (a >= b);
+                break;
+            default:
+                throw new EvaluationException(String.format("%s: undefined operand \"%s\"", this, operand));
+        }
+
+        return (r) ? 1 : 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", lhs, operand, rhs);
+    }
+}
