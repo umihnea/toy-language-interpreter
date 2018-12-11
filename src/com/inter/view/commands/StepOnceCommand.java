371698a270ef501b5e7c0081c39c19565fd1ce77
axtrm <umihnea52@gmail.com>
2018-12-11 17:15:54 +0200

    Update StepOnceCommand to call the new methods

--- src/com/inter/view/commands/StepOnceCommand.java
+++ src/com/inter/view/commands/StepOnceCommand.java
@@ -2,17 +2,16 @@ package com.inter.view.commands;
 
 import com.inter.controller.Controller;
 import com.inter.exceptions.InterpreterException;
-import com.inter.model.ProgramState;
 
 public class StepOnceCommand extends Command {
 
     private Controller controller;
-    private Boolean displayFlag = false;
+//    private Boolean displayFlag = false;
 
-    public StepOnceCommand(String key, String description, Controller controller, Boolean displayFlag) {
+    public StepOnceCommand(String key, String description, Controller controller) { //, Boolean displayFlag) {
         super(key, description);
         this.controller = controller;
-        this.displayFlag = displayFlag;
+//        this.displayFlag = displayFlag;
     }
 
 //    public StepOnceCommand(String key, String description, Controller controller) {
@@ -21,24 +20,21 @@ public class StepOnceCommand extends Command {
 //    }
 
     @Override
-    public void execute() throws InterpreterException {
-        ProgramState state = this.controller.getCurrentProgramState();
-
+    public void execute() throws InterpreterException, InterruptedException {
         if (!controller.hasProgram())
             throw new InterpreterException("No program loaded.");
-        else if (state.getStack().isEmpty())
-            throw new InterpreterException("Program already done.");
+//        else if (state.getStack().isEmpty())
+//            throw new InterpreterException("Program already done.");
 
-        state = controller.stepOnce(state);
-        this.controller.setCurrentProgramState(state);
+        controller.stepOnce();
         System.out.println("Stepped once.");
 
-        if (displayFlag)
-            System.out.println(state);
+//        if (displayFlag)
+//            System.out.println(state);
 
-        if (state.getStack().isEmpty()) {
-            System.out.println("Program finished. Output:");
-            System.out.println(state.getBuffer());
-        }
+//        if (state.getStack().isEmpty()) {
+//            System.out.println("Program finished. Output:");
+//            System.out.println(state.getBuffer());
+//        }
     }
 }
