cf60a1ecb3c4c88d4ba0139cdeaaad4a4880cdcb
axtrm <umihnea52@gmail.com>
2019-01-25 09:57:20 +0200

    Implemented new latch statement

--- src/com/inter/model/latch/NewLatchStatement.java
+++ src/com/inter/model/latch/NewLatchStatement.java
@@ -2,25 +2,32 @@ package com.inter.model.latch;
 
 import com.inter.exceptions.InterpreterException;
 import com.inter.model.ProgramState;
+import com.inter.model.expressions.ConstantExpression;
 import com.inter.model.expressions.Expression;
+import com.inter.model.statements.AssignmentStatement;
 import com.inter.model.statements.Statement;
 
 public class NewLatchStatement extends Statement {
     private String key;
-    private Expression expr;
+    private Expression expression;
+    private static int latchIndex;
 
-    public NewLatchStatement(String key, Expression expr) {
+    public NewLatchStatement(String key, Expression e) {
         this.key = key;
-        this.expr = expr;
+        this.expression = e;
     }
 
     @Override
     public ProgramState execute(ProgramState state) throws InterpreterException {
-        return null;
+        ILatchTable latchTable = state.getLatchTable();
+        int latchId = latchIndex + 1;
+        latchIndex++;
+        latchTable.put(latchId, this.expression.evaluate(state.getSymbolTable(), state.getHeap()));
+        return new AssignmentStatement(this.key, new ConstantExpression(latchId)).execute(state);
     }
 
     @Override
     public String toString() {
-        return String.format("writeHeap(%s, %s)", this.key, this.expr);
+        return String.format("writeHeap(%s, %s)", this.key, this.expression);
     }
 }
